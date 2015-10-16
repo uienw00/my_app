e058edcecd083dc2574c492e3f206c83d3aebd29
uienw00 <uienw00@gmail.com>
2015-10-16 19:14:26 +0900

    static test

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
 app.get('/', function (req,res){
   res.send('Hello World!');
 });
+*/
 
 app.listen(3000, function(){
   console.log('Server On!');
