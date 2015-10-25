457feade597b7b3508cc9a7675b572608f3df9bd
uienw00 <uienw00@gmail.com>
2015-10-25 21:48:11 +0900

    board add

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
@@ -12,6 +16,66 @@ db.on("error", function(err){
   console.log("DB ERROR :", err);
 });
 
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updateAt: Date
+});
+var Post = mongoose.model('post', postSchema);
+
+// view setting
+app.set("view engine", 'ejs');
+
+// set middleweares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+// set routes
+app.get('/posts', function(req,res){
+  Post.find({}).sort('-createdAt').exec(function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts});
+  });
+}); //index
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
+}); // new
+app.post('/posts', function(req,res){
+  console.log(req.body);
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect("/posts");
+  });
+}); // create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show",{data:post});
+  });
+}); //show
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+}); //edit
+app.put('/posts/:id', function(req,res){
+  req.body.post.updateAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect("/posts/"+req.params.id);
+  });
+}); // update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect("/posts");
+  });
+}); // destroy
 var dataSchema = mongoose.Schema({
   name:String,
   count:Number
@@ -27,8 +91,6 @@ Data.findOne({name:"myData"}, function(err,data){
   }
 });
 
-app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req,res){
   Data.findOne({name:"myData"}, function(err,data){
     if(err) return console.log("Data ERROR:", err);
